763550b0900af0bc501739d2ffdceb178d85dedc
Dihue <dihue21@gmail.com>
2024-11-03 20:13:58 -0300

    Recuperar lista de usuarios

--- src/apps/usuarios/models.py
+++ src/apps/usuarios/models.py
@@ -5,3 +5,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class Usuario(AbstractUser):
     dni = models.IntegerField(null=True)
+
+    # Redefinición del método string
+    def __str__(self):
+        return f"ID:{self.id} - Username:{self.username} - {self.first_name}"
--- src/apps/usuarios/views.py
+++ src/apps/usuarios/views.py
@@ -1,15 +1,37 @@
 from django.shortcuts import render
 
+from .models import Usuario
+
 # Create your views here.
 def pagina_usuarios(request):
-    lista_usuarios = [
-        {"nombre": "Lucas", "apellido": "Ibañez"},
-        {"nombre": "Federico", "apellido": "Aguirre"},
-        {"nombre": "Dihue", "apellido": "De Cuadra"},
-    ]
+    # Trae a todos los objetos de la tabla Usuario de la DB
+    usuarios = Usuario.objects.all() # filter(id__in=[1,2,3])
 
-    contexto = {
-        "todos_los_usuarios": lista_usuarios
+    # Trae solo a los objetos, que coinciden con los valores
+    # del filter de la tabla Usuario de la DB
+    # usuarios = Usuario.objects.filter(id__in=[1,2,3])
+
+    # Para solo traer al primer objeto de toda la lista de "usuarios"
+    u = Usuario.objects.filter(id=1).first()
+
+    print("|--> TODOS LOS USUARIOS")
+    if u is None:
+        print("No existe usuario")
+    else:
+        print(u)
+        print(u.username)
+    '''
+    print("|--> TODOS LOS USUARIOS")
+    print(usuarios)
+    print(usuarios.query)
+    print("|--> Longitud:", len(usuarios))
+    print("|--> Longitud:", usuarios.count())
+
+    # Al tratarse de un QuerySet puedo iterar
+    for us in usuarios:
+        print(us.last_login)
+    '''
+    ctx = {
+        "usuarios": usuarios
     }
-
-    return render(request,'lista_usuarios.html', contexto)
+    return render(request,'usuarios/lista.html', ctx)
