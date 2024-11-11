dd7cbe0f9517fc6ccd924374085c5102a8415922
Dihue <dihue21@gmail.com>
2024-11-10 21:58:25 -0300

    Mejoras en el administrador de Django

--- src/apps/vacunas/admin.py
+++ src/apps/vacunas/admin.py
@@ -2,6 +2,34 @@ from django.contrib import admin
 
 from .models import Vacuna, Dosis
 
-# Register your models here.
-admin.site.register(Vacuna)
-admin.site.register(Dosis)
+
+class DosisInline(admin.StackedInline):
+    model = Dosis
+    extra = 0
+
+
+class VacunaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'codigo', 'nombre', 'cant_dosis']
+    inlines = [DosisInline]
+    search_fields = ('codigo', 'nombre')
+
+
+admin.site.register(Vacuna, VacunaAdmin)
+
+
+class DosisAdmin(admin.ModelAdmin):
+    list_display = ['id', 'get_vacuna_codigo', 'get_vacuna_nombre', 'nombre', 'orden']
+    search_fields = ('vacuna__codigo', 'vacuna__nombre', 'nombre')
+
+    def get_vacuna_codigo(self, obj):
+        return obj.vacuna.codigo
+    
+    get_vacuna_codigo.short_description = "Código Vacuna"
+
+    def get_vacuna_nombre(self, obj):
+        return obj.vacuna.nombre
+    
+    get_vacuna_nombre.short_description = "Nombre Vacuna"
+
+
+admin.site.register(Dosis, DosisAdmin)
