97f935f260abe36e89f29333ebc571a2d87100b4
Dihue <dihue21@gmail.com>
2024-11-12 23:47:24 -0300

    CRUD de vacunas

--- src/apps/vacunas/urls.py
+++ src/apps/vacunas/urls.py
@@ -5,6 +5,16 @@ from . import views
 app_name = "vacunas"
 
 urlpatterns = [
-    #path('lista/', views.Lista.as_view(), name='lista'),
-    #path('nuevo/', views.Nuevo.as_view(), name='nuevo')
+    # Lista de Vacunas
+    path('lista/', views.Listar.as_view(), name='lista'),
+    # Crear nueva Vacuna
+    path('nuevo/', views.Nuevo.as_view(), name='nuevo'),
+    # Editar una Vacuna
+    path('vacuna/<int:pk>/editar/', views.VacunaUpdate.as_view(), name='editar'),
+    # Detalle Vacuna
+    path('vacuna/<int:pk>/', views.VacunaDetail.as_view(), name='detalle'),
+    # Eliminar Vacuna
+    path('vacuna/<int:pk>/eliminar', views.VacunaDeleteView.as_view(), name='eliminar')
+    # Búsqueda Vacuna
+    #TODO: hacer funcionar la búsqueda de vacunas por el nombre al menos
 ]
--- src/apps/vacunas/views.py
+++ src/apps/vacunas/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Vacuna, Dosis
+from .forms import FormVacuna
+
+class Listar(ListView):
+    template_name = 'vacunas/lista.html'
+    model = Vacuna
+    context_object_name = 'vacunas'
+    paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        ctx = super(Listar, self).get_context_data(**kwargs)
+        ctx["titulo"] = "Lista de vacunas"
+        return ctx
+
+
+class Nuevo(CreateView):
+    template_name = 'vacunas/nuevo.html'
+    model = Vacuna
+    form_class = FormVacuna
+    success_url = reverse_lazy("vacunas:lista")
+
+    def get_context_data(self, **kwargs):
+        ctx = super(Nuevo, self).get_context_data(**kwargs)
+        ctx["titulo"] = "Nueva vacuna"
+        return ctx
+
+
+class VacunaUpdate(UpdateView):
+    model = Vacuna
+    fields = ['codigo', 'nombre', 'cant_dosis']
+    template_name = 'vacunas/editar.html'
+    success_url = reverse_lazy('vacunas:lista')
+
+    def get_context_data(self, **kwargs):
+        ctx = super(VacunaUpdate, self).get_context_data(**kwargs)
+        ctx["titulo"] = "Editar vacuna"
+        return ctx
+
+
+class VacunaDetail(DetailView):
+    model = Vacuna
+    template_name = 'vacunas/detalle.html'
+    context_object_name = 'vacuna'
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['dosis'] = Dosis.objects.filter(vacuna=self.object)
+        return ctx
+
+
+class VacunaDeleteView(DeleteView):
+    model = Vacuna
+    template_name = 'vacunas/confirmar_eliminacion.html'
+    success_url = reverse_lazy('vacunas:lista')
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['dosis'] = Dosis.objects.filter(vacuna=self.object)
+        return ctx
