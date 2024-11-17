34535b4c7a589e2104952ab01616d0611d4d438b
Dihue <dihue21@gmail.com>
2024-11-17 16:19:26 -0300

    Uso de imagen de perfil

--- src/apps/usuarios/models.py
+++ src/apps/usuarios/models.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class Usuario(AbstractUser):
     dni = models.IntegerField(null=True)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
 
     # Redefinición del método string
     def __str__(self):
-        return f"ID: {self.id} - Username: {self.username} - {self.first_name}"
+        return f"{self.id} - {self.username} - {self.first_name}"
